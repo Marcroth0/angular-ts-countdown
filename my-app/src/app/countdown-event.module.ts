31c044f6e959e3e7f71804b05fc6ffc9c7184abc
Marcus Rothoff <marcusrothoff@MacBook-Pro-som-tillhor-Marcus.local>
2022-09-28 01:19:40 +0200

    import fittextmodule

--- my-app/src/app/countdown-event.module.ts
+++ my-app/src/app/countdown-event.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Ng2FittextModule } from "ng2-fittext";
 
 import { NgbdDatepickerPopup } from './countdown-event';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, NgbModule,],
+  imports: [BrowserModule, FormsModule, NgbModule, Ng2FittextModule],
   declarations: [NgbdDatepickerPopup],
   exports: [NgbdDatepickerPopup],
   bootstrap: [NgbdDatepickerPopup]
